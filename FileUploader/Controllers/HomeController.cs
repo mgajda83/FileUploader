3ccdb62a82f950699483ed91ac01761bd2df7d60
Michal Gajda <mgajda83@gmail.com>
2022-07-09 10:12:38 +0200

    InitialUpload

---
+++ FileUploader/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace FileUploader.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            ViewData["RequestId"] = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+            return View();
+        }
+    }
+}
